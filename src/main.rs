10a290b8fba5c5fafddf2a666735f434d8220bec
ColtG-28 <Colt09@icloud.com>
2025-08-31 20:55:09 -0700

    Auto commit

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ mod gui;
 fn main() {
     println!("Hello, World!");
     println!("Ok maybe this works");
-    gui::gui();
+    let _ = gui::gui();
     // Run the info collect and the analysis here...
 }
