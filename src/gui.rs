74db7816df6554caaae2caf24ff6e03bd10ef4a1
ColtG-28 <Colt09@icloud.com>
2025-08-31 20:44:21 -0700

    Auto commit

--- src/gui.rs
+++ src/gui.rs
@@ -1,22 +1,78 @@
-use eframe::*;
-use egui::{CentralPanel, Ui};
+use eframe::egui;
+use eframe::egui::{CentralPanel, Ui};
 
-struct MyApp {}
+pub fn gui() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+    )
+}
 
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
-        CentralPanel::default().show(ctx, |ui: &mut Ui| {
-            ui.label("Hello, World!");
-        });
+struct MyApp {
+    my_string: String,
+    my_f32: f32,
+    my_boolean: bool,
+    my_enum: Enum,
+    my_image: egui::TextureId,
+}
+
+#[derive(PartialEq)]
+enum Enum {
+    First,
+    Second,
+    Third,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            my_string: "Hello".to_owned(),
+            my_f32: 42.0,
+            my_boolean: false,
+            my_enum: Enum::First,
+            // For demo purposes, just use a placeholder texture id.
+            // In a real app, you’d load an image into a texture.
+            my_image: egui::TextureId::Managed(0),
+        }
     }
 }
 
-pub fn gui() -> eframe::Result<()> {
-    let native_options = eframe::NativeOptions::default();
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.add(egui::Label::new("Hello World!"));
+            ui.label("A shorter and more convenient way to add a label.");
+            if ui.button("Click me").clicked() {
+                // take some action here
+                self.my_boolean = !self.my_boolean;
+            }
 
-    eframe::run_native(
-        "ProjectX Security System",
-        native_options,
-        Box::new(|_cc: &CreationContext<'_>| Ok(Box::new(MyApp {}))),
-    )
+            ui.label("This is a label");
+            ui.hyperlink("https://github.com/emilk/egui");
+            ui.text_edit_singleline(&mut self.my_string);
+
+            if ui.button("Click me again").clicked() {}
+
+            ui.add(egui::Slider::new(&mut self.my_f32, 0.0..=100.0));
+            ui.add(egui::DragValue::new(&mut self.my_f32));
+
+            ui.checkbox(&mut self.my_boolean, "Checkbox");
+
+            ui.horizontal(|ui| {
+                ui.radio_value(&mut self.my_enum, Enum::First, "First");
+                ui.radio_value(&mut self.my_enum, Enum::Second, "Second");
+                ui.radio_value(&mut self.my_enum, Enum::Third, "Third");
+            });
+
+            ui.separator();
+
+            ui.image((self.my_image, egui::Vec2::new(640.0, 480.0)));
+
+            ui.collapsing("Click to see what is hidden!", |ui| {
+                ui.label("Not much, as it turns out");
+            });
+        });
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,3 @@ fn main() {
     gui::gui();
     // Run the info collect and the analysis here...
 }
-
-// The following function will run through each area of the file collecting the data to fill out the struct essentially. 
-fn collect_file_info(new_file: File) -> File{
-    println!("Collecting information for file: {}", new_file.name);
-    println!("File size: {} bytes", new_file.size);
-    println!("File type: {}", new_file.file_type);
-    println!("File hash: {}", new_file.hash);
-    println!("File header: {}", new_file.header);
-    println!("File contents: {:?}", new_file.contents);
-    new_file.clone()
-}
-struct File {
-    name: String,
-    size: u64,
-    file_type: String,
-    hash: String,
-    header: String,
-    contents: Vec<u8>, // Might want to switch this to something else in order to store the remaining file contents.
-}
-
-// The following will be used to run through the tests on each separate area of the file, returning a boolean of the file's safety.
-fn analyze_file(new_file: &File) -> bool {
-    println!("Analyzing file: {}", new_file.name);
-    // Perform analysis on the file contents
-    // Will return false if the file isn't safe. 
-    /* 
-        Ensure that the file is analyzed piece by piece and allowing for a false return after each portion is analyzed to ensure the shortest
-        possible lifetime of the file on each device.
-    */
-    true
-}
